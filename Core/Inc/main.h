7d9caa78afaf1bce8effd2f59da12fcc59ad140a
Lllcccc712 <1584750243@qq.com>
2026-08-11 14:40:08 +0800

    配置

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,6 +57,16 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED_1_Pin GPIO_PIN_4
+#define LED_1_GPIO_Port GPIOA
+#define LED_2_Pin GPIO_PIN_5
+#define LED_2_GPIO_Port GPIOA
+#define LED_3_Pin GPIO_PIN_6
+#define LED_3_GPIO_Port GPIOA
+#define LED_4_Pin GPIO_PIN_7
+#define LED_4_GPIO_Port GPIOA
+#define BEEP_Pin GPIO_PIN_8
+#define BEEP_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
